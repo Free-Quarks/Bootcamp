f8754b2c5a36ee3f7527114bc115f4a74d8ad4e4
Justin Lieffers <lieffers@email.arizona.edu>
2021-03-11 16:12:45 -0700

    added some comments

--- c_assignments/c_assigment_1/c_assignment_1.c
+++ c_assignments/c_assigment_1/c_assignment_1.c
@@ -1,21 +1,28 @@
-/* Need to comment code */
+/* This program adds the integrers of a larger integer together using a
+ * recursive algorithm instead of loops */
 
 #include <stdio.h>
 
+// This first function here wis what does the adding recusively
 int sum_digits(int x)
 {
-    if (x < 10) {
+    // Here for the last digit, less than 10, we just return the digit
+    if (x < 10) { 
         return x;
     }
+    // Here we have the recursion moving through the digits 1 at a time 
     else {
         return ((x % 10) + sum_digits(x / 10));
     }
 }
 
+// This is the main function which gets the input from the console 
 int main( void )
 {
+    // getting the input
     int y;
     scanf("%d", &y);
+    // printing the result 
     printf("Sum of digits = %d\n", sum_digits(y));
     return 0;
 }
